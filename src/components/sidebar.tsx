6ccb07d971ad6a6237325298d2c1aef66fd10685
Shittu Ibrahim <2bitspanky2000@gmail.com>
2025-08-19 21:30:14 +0100

    Close sidebar on click, listening for a click

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,31 +1,33 @@
 import { Routes } from "@/lib/routes"
 import { ChevronRight, LogOutIcon } from "lucide-react"
 import Image from "next/image"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation" // ✅ added useRouter
 
 const SideBar = ({ onLinkClick }: { onLinkClick?: () => void }) => {
-console.warn(onLinkClick)
     const pathname = usePathname();
-    
+    const router = useRouter(); // ✅ initialize router
+
     return (
         <div className="bg-[#ffffff] w-full border-r border-[#EDEEF3] px-[18px] py-[30px] flex flex-col gap-14">
             <Image src={'/logo.svg'} alt="Logo" width={137.7} height={28} />
-            
+
             <div className="flex w-full h-full flex-col items-center justify-between">
                 <div className="routes w-full flex flex-col gap-3">
                     {Routes.map((route) => {
                         const isActive = pathname.startsWith(route.route);
                         return (
-                            <Link
+                            <div
                                 key={route.route}
-                                href={route.route}
+                                onClick={() => {
+                                    onLinkClick?.();         // ✅ close sidebar
+                                    router.push(route.route) // ✅ navigate
+                                }}
                                 className={`route cursor-pointer rounded-xl w-full items-center px-4 py-3 flex gap-[10px] font-medium text-sm
                                 ${isActive ? 'bg-secondary text-primary' : 'text-[#1A1A1A] hover:bg-secondary hover:text-primary'}`}
                             >
                                 <route.icon />
                                 <span>{route.name}</span>
-                            </Link>
+                            </div>
                         );
                     })}
                 </div>
